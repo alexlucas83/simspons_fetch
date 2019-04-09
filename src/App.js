85bd1130f068b72fa75f63e73c17e8388e83af78
alexlucas <alex.plomberie83@gmail.com>
2019-04-09 15:47:54 +0200

    simspons_fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  GenerateEmployee  from  './component/GenerateEmployee';
+import  DisplayEmployee  from  './component/DisplayEmployee';
+
+const  sampleEmployee = {
+  quote: "Shoplifting is a victimless crime, like punching someone in the dark.",
+  character: "Nelson Muntz",
+  image : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+  characterDirection : "Left"
+};
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // on peut mettre notre sampleEmployee par défaut
+      // afin d'avoir toujours un employé d'affiché
+      employee:  sampleEmployee
+    };
+  }
+  getEmployee() {
+    // Récupération de l'employé via fetch
+    fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then(response  =>  response.json())
+      .then(data  => {
+        // Une fois les données récupérées, on va mettre à jour notre state avec les nouvelles données
+        this.setState({
+          employee:  data[0],
+        });
+    });
+}
+
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src='https://i.pinimg.com/originals/32/be/28/32be28afd7f30e8cc86b51ad7d909905.png' className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -20,6 +55,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        {/* <GenerateEmployee /> */}
+        {/* <DisplayEmployee employee={sampleEmployee} /> */}
+        <GenerateEmployee  selectEmployee={() =>  this.getEmployee()}  />
+        <DisplayEmployee  employee={this.state.employee}  />
       </div>
     );
   }
